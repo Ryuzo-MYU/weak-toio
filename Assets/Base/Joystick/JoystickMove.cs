028c2884bea8556b344bc538ccc0aaaf3c2e4572
Ryuzo-MYU <p2120165@myu.ac.jp>
2024-07-31 19:19:29 +0900

    toioの動作に関わる処理をJoystickMoveに集約． LED機能を実装しようとしたがラグの関係で断念．今後の実装に期待

--- Assets/Base/Joystick/JoystickMove.cs
+++ Assets/Base/Joystick/JoystickMove.cs
@@ -16,6 +16,8 @@ public class JoystickMove : MonoBehaviour
 
 	public int leftWheel;
 	public int rightWheel;
+	public int[] soundID = new int[4];
+	public bool[] rgb = new bool[3] { false, false, false };
 	async void Start()
 	{
 		// ConnectType.Auto - ビルド対象に応じて内部実装が自動的に変わる
@@ -27,18 +29,20 @@ public class JoystickMove : MonoBehaviour
 
 	void Update()
 	{
-		MoveOperation();
-		RotateOperation();
+		MoveCalcurate();
+		RotateCalcurate();
 
 		// CubeManagerからモジュールを間接利用した場合:
 		foreach (var cube in cm.syncCubes)
 		{
+			SoundOperation(cube);
+			// LEDOperation(cube);
 			cube.Move(leftWheel, rightWheel, duration);
 		}
 	}
 
 	// toioを前後左右に動かす
-	void MoveOperation()
+	void MoveCalcurate()
 	{
 		float moveHorizontal = joystick.moveHorizontal;
 		float moveVertical = joystick.moveVertical;
@@ -60,7 +64,7 @@ public class JoystickMove : MonoBehaviour
 	}
 
 	// toioをその場で回転させる
-	void RotateOperation()
+	void RotateCalcurate()
 	{
 		float rotateButton = joystick.rotateButton;
 		float moveSpeed = wheelSpeed.moveSpeed;
@@ -74,5 +78,61 @@ public class JoystickMove : MonoBehaviour
 			leftWheel = (int)(moveSpeed * rotateButton);
 			rightWheel = (int)(moveSpeed * -rotateButton);
 		}
+	}
+	void SoundOperation(Cube cube)
+	{
+
+		int[] soundid = soundID;
+		if (Input.GetButton("Sound Button 0"))
+		{
+			Debug.Log("Sound Buttom 1 Pushed!");
+			cube.PlayPresetSound(soundid[0], 255, Cube.ORDER_TYPE.Strong);
+			Debug.Log("Sound Play Success!");
+		}
+
+		if (Input.GetButton("Sound Button 1"))
+		{
+			Debug.Log("Sound Button 1 Pushed!");
+			cube.PlayPresetSound(soundid[1], 255, Cube.ORDER_TYPE.Strong);
+			Debug.Log("Sound Play Success!");
+		}
+		if (Input.GetButton("Sound Button 2"))
+		{
+			Debug.Log("Sound Button 2 Pushed!");
+			cube.PlayPresetSound(soundid[2], 255, Cube.ORDER_TYPE.Strong);
+			Debug.Log("Sound Play Success!");
+		}
+		if (Input.GetButton("Sound Button 3"))
+		{
+			Debug.Log("Sound Button 3 Pushed!");
+			cube.PlayPresetSound(soundid[3], 255, Cube.ORDER_TYPE.Strong);
+			Debug.Log("Sound Play Success!");
+		}
+	}
+	void LEDOperation(Cube cube)
+	{
+		if (Input.GetButton("LED Button R"))
+		{
+			Debug.Log("LED Button R Pushed!");
+			rgb = new bool[3] { rgb[0], false, false };
+			rgb[0] = !rgb[0];
+		}
+		if (Input.GetButton("LED Button G"))
+		{
+			Debug.Log("LED Button G Pushed!");
+			rgb = new bool[3] { false, rgb[0], false };
+			rgb[1] = !rgb[1];
+		}
+		if (Input.GetButton("LED Button B"))
+		{
+			Debug.Log("LED Button B Pushed!");
+			rgb = new bool[3] { false, false, rgb[0] };
+			rgb[2] = !rgb[2];
+		}
+
+		if (rgb[0]) { cube.TurnLedOn(255, 0, 0, 10); }
+		else if (rgb[1]) { cube.TurnLedOn(0, 255, 0, 10); }
+		else if (rgb[2]) { cube.TurnLedOn(0, 0, 255, 10); }
+		else { cube.TurnLedOff(); }
 	}
 }
