d1679916f94c58a8701ac8d8a9d66bd90e144d61
Ryuzo-MYU <p2120165@myu.ac.jp>
2024-11-21 23:52:25 +0000

    2024年11月22日 8:52

---
+++ Assets/weak-toio/Scripts/EvaluateEnvironment/Evaluation/EvaluationResultSender.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace weak-toio.Assets.weak-toio.Scripts.EvaluateEnvironment.Evaluation
+{
+    public interface EvaluationResultSender
+    {
+        
+    }
+}
