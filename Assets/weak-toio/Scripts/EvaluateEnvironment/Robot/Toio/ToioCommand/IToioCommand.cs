acd41bb1a04d6f940f5ae511b2b54e3ce71b3347
Ryuzo-MYU <p2120165@myu.ac.jp>
2024-11-29 06:07:26 +0000

    2024年11月29日 15:07

--- Assets/weak-toio/Scripts/EvaluateEnvironment/Robot/Toio/ToioCommand/IToioCommand.cs
+++ Assets/weak-toio/Scripts/EvaluateEnvironment/Robot/Toio/ToioCommand/IToioCommand.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Robot
 {
 	public interface IToioCommand
 	{
-public 
+		public void Execute(Toio toio);
 	}
 }
---
+++ Assets/weak-toio/Scripts/EvaluateEnvironment/Robot/Toio/ToioCommand/TranslateCommand.cs
@@ -0,0 +1,21 @@
+using toio;
+
+namespace Robot
+{
+	public class TranslateCommand : IToioCommand
+	{
+		float dist;
+		double speed;
+		public TranslateCommand(float _dist, double _speed)
+		{
+			dist = _dist;
+			speed = _speed;
+		}
+		public void Execute(Toio toio)
+		{
+			CubeHandle handle = toio.Handle;
+			Movement movement = handle.TranslateByDist(dist, speed);
+			handle.Move(movement);
+		}
+	}
+}
