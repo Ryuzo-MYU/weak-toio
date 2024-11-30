af53d1dc7d1d293b839fc197494b129ef30153ee
Ryuzo-MYU <p2120165@myu.ac.jp>
2024-11-30 02:43:30 +0000

    2024年11月30日 11:43

--- Assets/M5Stick/SendSensorInfo/.pio/build/m5stick-c/FrameworkArduino/HWCDC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/m5stick-c/FrameworkArduino/HWCDC.cpp.o: \
- C:/Users/donch/.platformio/packages/framework-arduinoespressif32/cores/esp32/HWCDC.cpp \
- C:/Users/donch/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- C:/Users/donch/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- Assets/M5Stick/SendSensorInfo/.pio/build/m5stick-c/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/m5stick-c/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/donch/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/donch/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
