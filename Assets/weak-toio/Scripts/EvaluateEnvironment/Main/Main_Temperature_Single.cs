37bc84e3f619081426b7c3f4aeb8c23757675117
Ryuzo-MYU <p2120165@myu.ac.jp>
2024-12-01 01:28:02 +0000

    2024年12月1日 10:28

--- Assets/weak-toio/Scripts/EvaluateEnvironment/Main/Main_Temperature_Single.cs
+++ Assets/weak-toio/Scripts/EvaluateEnvironment/Main/Main_Temperature_Single.cs
@@ -45,11 +45,14 @@ public class Main_Temperature_Single : MonoBehaviour
 		{
 			// toioに接続
 			cubeManager = new CubeManager(connectType);
-			await cubeManager.SingleConnect();
+			await cubeManager.MultiConnect(cubeCount);
 			connected = true;
 			Debug.Log("接続完了");
 
-			toio = new Toio(0, cubeManager);
+			for (int id = 0; id < cubeManager.connectedCubes.Count; id++)
+			{
+				if (!cubeManager.connectedCubes[id].isConnected) { toio = new Toio(id, cubeManager); }
+			}
 		}
 		catch (Exception e)
 		{
