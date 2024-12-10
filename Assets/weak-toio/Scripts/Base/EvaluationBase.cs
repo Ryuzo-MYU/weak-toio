9060ea5921ba90de585d9a9637862b19e65df844
Ryuzo-MYU <p2120165@myu.ac.jp>
2024-12-10 05:23:54 +0000

    2024年12月10日 14:23

--- Assets/weak-toio/Scripts/Base/EvaluationBase.cs
+++ Assets/weak-toio/Scripts/Base/EvaluationBase.cs
@@ -1,5 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using Environment;
+using Evaluation;
+using Robot;
 using UnityEngine;
 
-public class EvaluationBase<TSensor, TEvaluate, TActionGenerator> : MonoBehaviour {
-		
+public abstract class EvaluationBase<TSensor, TEvaluate, TActionSender> : MonoBehaviour
+	where TSensor : ISensorUnit
+	where TEvaluate : EvaluationResultSender<TSensor>
+	where TActionSender : ActionSender
+{
+	[SerializeField] protected string toioName;
+	[SerializeField] protected string attachedToioName;
+	public ToioConnector connector;
+	public string PORTNAME = "COM9";
+	public int BAUDRATE = 115200;
+	protected SerialHandler serial;
+	protected TSensor sensor;
+	public bool UseDummy;
+	public EnvType envType = EnvType.NotAppointed;
+	protected TEvaluate evaluator;
+	protected TActionSender actionGenerator;
+	protected bool connected = false;
+	protected Toio toio;
+
+	protected virtual void Awake()
+	{
+		connector.OnConnectSuccessed += OnConnectSuccessed;
+		InitializeSerial();
+		InitializeSensor();
+		InitializeEvaluator();
 	}
+
+	protected virtual void Start()
+	{
+		sensor.Start();
+	}
+
+	protected virtual void Update()
+	{
+		serial.Update();
+		sensor.Update();
+	}
+
+	protected virtual void OnConnectSuccessed(List<Toio> toios)
+	{
+		Debug.Log("接続開始");
+		toio = toios.Find(t => t.Name == toioName);
+		attachedToioName = toio.Name;
+		toio.EnvType = sensor.GetEnvType();
+		connected = true;
+		StartCoroutine(UpdateEvaluate());
+	}
+
+	protected abstract void InitializeSensor();
+	protected abstract void InitializeEvaluator();
+
+	private void InitializeSerial()
+	{
+		serial = new SerialHandler(PORTNAME, BAUDRATE);
+		UseDummy = !serial.Awake();
+	}
+
+	protected IEnumerator UpdateEvaluate()
+	{
+		while (true)
+		{
+			if (!connected && sensor == null)
+			{
+				yield return new WaitForSeconds(0.1f);
+				continue;
+			}
+
+			Result result = evaluator.GetEvaluationResult(sensor);
+			Robot.Action action = actionGenerator.GenerateAction(result);
+
+			if (!toio.AddNewAction(action))
+			{
+				Debug.LogWarning("アクション溜まってんね");
+			}
+
+			yield return StartCoroutine(toio.Move());
+		}
+	}
+}
