52cd7a08b37ff9cee99ce574517284b5bfb377a7
Ryuzo-MYU <p2120165@myu.ac.jp>
2024-12-10 05:29:04 +0000

    2024年12月10日 14:29

--- Assets/weak-toio/Scripts/EvaluateScene/Temperature/Main_Temperature.cs
+++ Assets/weak-toio/Scripts/EvaluateScene/Temperature/Main_Temperature.cs
@@ -7,28 +7,10 @@ using Robot;
 using toio;
 using UnityEngine;
 
-public class Main_Temperature : MonoBehaviour
+public class Main_Temperature : EvaluationBase<ITemperatureSensor, TemperatureEvaluate, TemperatureActionGenerator>
 {
-	[Tooltip("接続したいtoioの固有名\n事前にローカルネームを出力するテンプレートなどで名前を把握しておくこと")]
-	[SerializeField] string toioName;
-	[SerializeField] string attachedToioName;
-	public ToioConnector connector;
-	public string PORTNAME = "COM9";
-	public int BAUDRATE = 115200;
-	SerialHandler serial;
-	ITemperatureSensor sensor;
-	public bool UseDummy;
-	[SerializeField] TempBoundary tempBoundary;
-	public EnvType envType = EnvType.NotAppointed;
-	TemperatureEvaluate tempEval;
-	ActionSender tempAction;
-	private bool connected = false;
-	Toio toio;
-	[SerializeField] float currentTemp;
-
-	private void Awake()
+	protected override void InitializeSensor()
 	{
-		connector.OnConnectSuccessed += OnConnectSuccessed;
 		// センサー系の初期化
 		serial = new SerialHandler(PORTNAME, BAUDRATE);
 		// シリアルポートの初期化が失敗したらダミーを使う
@@ -36,54 +18,9 @@ public class Main_Temperature : MonoBehaviour
 		sensor = new M5TemperatureSensor(serial);
 		if (UseDummy) sensor = new DummyTemperatureSensor();
 		envType = sensor.GetEnvType();
-
-		// 評価システムの初期化
-		tempEval = new TemperatureEvaluate(tempBoundary.UpperBound, tempBoundary.LowerBound);
-		tempAction = new TemperatureActionGenerator();
 	}
-	private void Start()
+	protected override void InitializeEvaluator()
 	{
-		sensor.Start();
-	}
-	private void Update()
-	{
-		serial.Update();
-		sensor.Update();
-	}
-	private void OnConnectSuccessed(List<Toio> toios)
-	{
-		Debug.Log("接続開始");
-		if (connector.connectType == ConnectType.Real) toio = toios.Find(t => t.Name == toioName);
-		else toio = toios.Find(t => t.Name == this.gameObject.name);
-		attachedToioName = toio.Name;
-		toio.EnvType = sensor.GetEnvType(); // 役割を割り当て
-		connected = true;
-		StartCoroutine(UpdateEvaluate());
-	}
-	IEnumerator UpdateEvaluate()
-	{
-		while (true)
-		{
-			if (!connected && sensor == null)
-			{
-				yield return new WaitForSeconds(0.1f);
-				continue;
-			}
-			currentTemp = sensor.GetTemperature();
-			Result result = tempEval.GetEvaluationResult(sensor);
-			Robot.Action action = tempAction.GenerateAction(result);
-			if (!toio.AddNewAction(action))
-			{
-				Debug.LogWarning("アクション溜まってんね");
-			}
-			yield return StartCoroutine(toio.Move());
-		}
-	}
-
-	[Serializable]
-	struct TempBoundary
-	{
-		public float UpperBound;
-		public float LowerBound;
+		
 	}
 }
