9ba83ed840435e5650ad23f126fbb3965ba68cfd
Ryuzo-MYU <p2120165@myu.ac.jp>
2024-12-10 07:14:57 +0000

    2024年12月10日 16:14

--- Assets/weak-toio/Scripts/Base/Robot/Toio/ToioNameDisplay.cs
+++ Assets/weak-toio/Scripts/Base/Robot/Toio/ToioNameDisplay.cs
@@ -7,13 +7,21 @@ using System.Collections.Generic;
 public class ToioNameListUI : MonoBehaviour
 {
 	[SerializeField]
-	private TMP_Text[] text;
+	private GameObject textUI;
 	[SerializeField] ToioConnector toioConnector;
-	private void Start() {
-		toioConnector.OnConnectSucceeded += OnConnectSucessed;	
+	[SerializeField] GameObject contentParent;
+	private void Start()
+	{
+		toioConnector.OnConnectSucceeded += OnConnectSucessed;
 	}
 	private void OnConnectSucessed(List<Toio> toios)
 	{
-
+		foreach (Toio toio in toios)
+		{
+			string name = toio.Name;
+			TMP_Text text = textUI.GetComponent<TMP_Text>();
+			text.text = name;
+			Instantiate(textUI, contentParent.transform);
+		}
 	}
 }
