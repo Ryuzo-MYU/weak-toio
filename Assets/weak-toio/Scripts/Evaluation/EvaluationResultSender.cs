bf71a93d26e7691303b647333df2be67e06d1113
Ryuzo-MYU <p2120165@myu.ac.jp>
2024-12-14 00:57:25 +0000

    2024年12月14日 9:57

--- Assets/weak-toio/Scripts/Evaluation/EvaluationResultSender.cs
+++ Assets/weak-toio/Scripts/Evaluation/EvaluationResultSender.cs
@@ -1,11 +1,11 @@
-using System.Numerics;
-using Environment;
+using System.Collections.Generic;
 
 namespace Evaluation
 {
 	public interface EvaluationResultSender<T>
 	{
 		public Result GetEvaluationResult(T sensor);
+		public List<EnvType> GetEnvTypes();
 	}
 
 }
--- Assets/weak-toio/Scripts/Lib/BoundaryRange.cs
+++ Assets/weak-toio/Scripts/Lib/BoundaryRange.cs
@@ -1,31 +1,28 @@
-namespace Evaluation
+public struct BoundaryRange
 {
-	public struct BoundaryRange
+	public float UpperLimit;
+	public float LowerLimit;
+
+	/// <summary>
+	/// 値の範囲が1つしかない場合 
+	/// </summary>
+	/// <param name="targotParam"></param>
+	public BoundaryRange(float targotParam)
 	{
-		public float UpperLimit;
-		public float LowerLimit;
+		UpperLimit = targotParam;
+		LowerLimit = targotParam;
+	}
+	public BoundaryRange(float upper, float lower)
+	{
+		UpperLimit = upper;
+		LowerLimit = lower;
+	}
+	public bool isWithInRange(float subject)
+	{
+		// 値域に入る値が一つしかなければ、上限下限どちらかに等しいか判定
+		if (UpperLimit == LowerLimit) return subject == UpperLimit;
 
-		/// <summary>
-		/// 値の範囲が1つしかない場合 
-		/// </summary>
-		/// <param name="targotParam"></param>
-		public BoundaryRange(float targotParam)
-		{
-			UpperLimit = targotParam;
-			LowerLimit = targotParam;
-		}
-		public BoundaryRange(float upper, float lower)
-		{
-			UpperLimit = upper;
-			LowerLimit = lower;
-		}
-		public bool isWithInRange(float subject)
-		{
-			// 値域に入る値が一つしかなければ、上限下限どちらかに等しいか判定
-			if (UpperLimit == LowerLimit) return subject == UpperLimit;
-
-			// 複数あれば通常通り判定
-			return subject > LowerLimit && subject < UpperLimit;
-		}
+		// 複数あれば通常通り判定
+		return subject > LowerLimit && subject < UpperLimit;
 	}
 }
