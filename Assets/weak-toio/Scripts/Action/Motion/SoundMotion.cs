1077fe43d366e004f3075ec8ac661baaec03903c
Ryuzo-MYU <p2120165@myu.ac.jp>
2024-12-16 02:59:52 +0000

    2024年12月16日 11:59

--- Assets/weak-toio/Scripts/Action/Motion/SoundMotion.cs
+++ Assets/weak-toio/Scripts/Action/Motion/SoundMotion.cs
@@ -4,9 +4,10 @@ namespace Robot
 	{
 		public ISoundCommand SoundCommand { get; private set; }
 
-		public SoundMotion(ISoundCommand _sound) : base()
+		public SoundMotion(ISoundCommand _sound, float _interval) : base()
 		{
 			SoundCommand = _sound;
+			Interval = _interval;
 		}
 
 		public override void Exec(Toio toio)
