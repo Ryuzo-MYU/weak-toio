ce4e19a613168e6cfe4cd247ebd083263eed2b44
Ryuzo-MYU <p2120165@myu.ac.jp>
2024-12-22 04:43:00 +0000

    2024年12月22日 13:43

--- Assets/weak-toio/Samples/02_Glass_CO2/GrassCO2ActionGenerator.cs
+++ Assets/weak-toio/Samples/02_Glass_CO2/GrassCO2ActionGenerator.cs
@@ -3,24 +3,24 @@ namespace Robot
 {
 	public class GrassCO2ActionGenerator : ActionGenerator
 	{
-		BoundaryRange suitableRange = new BoundaryRange(0);
-		BoundaryRange cautionRange = new BoundaryRange(-10, 10);
+		BoundaryRange normalRange = new BoundaryRange(-2, 2);    // 通常範囲
+		BoundaryRange activeRange = new BoundaryRange(2, 10);    // 活発な光合成範囲
 
 		protected override Action GenerateAction(Result result)
 		{
 			float score = result.Score;
 
-			if (suitableRange.isWithInRange(score))
+			if (score < normalRange.LowerLimit)
 			{
-				return ToioActionLibrary.Grass_Normal();
+				return ToioActionLibrary.Grass_Wilting();        // CO2不足でしおれ気味
 			}
-			else if (score < 0)
+			else if (normalRange.isWithInRange(score))
 			{
-				return ToioActionLibrary.Grass_Wilting();
+				return ToioActionLibrary.Grass_Normal();         // 通常の生育状態
 			}
 			else
 			{
-				return ToioActionLibrary.Grass_Refreshed();
+				return ToioActionLibrary.Grass_Refreshed();      // 活発な光合成による成長
 			}
 		}
 	}
--- Assets/weak-toio/Samples/02_Glass_CO2/GrassCO2Evaluate.cs
+++ Assets/weak-toio/Samples/02_Glass_CO2/GrassCO2Evaluate.cs
@@ -1,44 +1,44 @@
-#region 草（湿度）
-
 using Environment;
-using Evaluation;
 using UnityEngine;
 
-[RequireComponent(typeof(IHumiditySensor))]
-public class GrassCO2Evaluate : EvaluateBase, IEvaluationResultSender<IHumiditySensor>
+namespace Evaluation
 {
-	[SerializeField] private BoundaryRange suitableRange = new BoundaryRange(40, 60); // 草の適正湿度
-	private IHumiditySensor humiditySensor;
-
-	public void GenerateEvaluationResult(IHumiditySensor humiditySensor)
+	[RequireComponent(typeof(ICO2Sensor))]
+	public class GrassCO2Evaluate : EvaluateBase, IEvaluationResultSender<ICO2Sensor>
 	{
-		_currentParam = humiditySensor.GetHumidity();
+		[SerializeField] private float OPTIMAL_CO2 = 400.0f;    // 通常大気のCO2濃度(ppm)
+		[SerializeField] private float MAX_EFFECT = 1200.0f;    // 光合成が最大になるCO2濃度(ppm)
+		private ICO2Sensor co2Sensor;
 
-		if (suitableRange.isWithInRange(_currentParam))
+		public void GenerateEvaluationResult(ICO2Sensor co2Sensor)
 		{
-			_score = 0;
+			_currentParam = co2Sensor.GetCO2();
+
+			if (_currentParam <= OPTIMAL_CO2)
+			{
+				_score = (_currentParam - OPTIMAL_CO2) / OPTIMAL_CO2 * 10; // CO2が少ない場合はマイナス評価
+			}
+			else if (_currentParam <= MAX_EFFECT)
+			{
+				_score = (_currentParam - OPTIMAL_CO2) / (MAX_EFFECT - OPTIMAL_CO2) * 10; // CO2増加で比例的に評価上昇
+			}
+			else
+			{
+				_score = 10; // 最大効果
+			}
+
+			_OnResultGenerated(new Result(_score, _unit));
 		}
-		else if (_currentParam < suitableRange.LowerLimit)
+
+		protected override void OnSensorDecided()
 		{
-			_score = _currentParam - suitableRange.LowerLimit;
+			base.OnSensorDecided();
+			co2Sensor = (ICO2Sensor)sensorManager.GetSensor();
 		}
-		else
+
+		protected override void OnDeserializeCompleted()
 		{
-			_score = _currentParam - suitableRange.UpperLimit;
+			GenerateEvaluationResult(co2Sensor);
 		}
-
-		_OnResultGenerated(new Result(_score, _unit));
-	}
-
-	protected override void OnSensorDecided()
-	{
-		base.OnSensorDecided();
-		humiditySensor = (IHumiditySensor)sensorManager.GetSensor();
-	}
-
-	protected override void OnDeserializeCompleted()
-	{
-		GenerateEvaluationResult(humiditySensor);
 	}
 }
-#endregion
