d818ef1c0726be70df2ae9ab04c23eaa13dacc4c
Ryuzo-MYU <p2120165@myu.ac.jp>
2024-12-22 04:33:53 +0000

    2024年12月22日 13:33

--- Assets/weak-toio/Samples/00_Human_Pa/HumanPaEvaluate.cs
+++ Assets/weak-toio/Samples/00_Human_Pa/HumanPaEvaluate.cs
@@ -1,39 +1,41 @@
 using Environment;
-using Evaluation;
 using UnityEngine;
 
-[RequireComponent(typeof(ICO2Sensor))]
-public class HumanPaEvaluate : EvaluateBase, IEvaluationResultSender<IPressureSensor>
+namespace Evaluation
 {
-	[SerializeField] private float baselinePressure = 1013.0f; // 基準気圧(hPa)
-	[SerializeField] private float warningThreshold = 10.0f;   // 警戒しきい値(±hPa)
-	private IPressureSensor pressureSensor;
-
-	public void GenerateEvaluationResult(IPressureSensor pressureSensor)
+	[RequireComponent(typeof(ICO2Sensor))]
+	public class HumanPaEvaluate : EvaluateBase, IEvaluationResultSender<IPressureSensor>
 	{
-		_currentParam = pressureSensor.GetPressure();
-		float pressureDiff = _currentParam - baselinePressure;
+		[SerializeField] private float baselinePressure = 1013.0f; // 基準気圧(hPa)
+		[SerializeField] private float warningThreshold = 10.0f;   // 警戒しきい値(±hPa)
+		private IPressureSensor pressureSensor;
 
-		if (Mathf.Abs(pressureDiff) <= warningThreshold)
+		public void GenerateEvaluationResult(IPressureSensor pressureSensor)
 		{
-			_score = 0; // 快適範囲
+			_currentParam = pressureSensor.GetPressure();
+			float pressureDiff = _currentParam - baselinePressure;
+
+			if (Mathf.Abs(pressureDiff) <= warningThreshold)
+			{
+				_score = 0; // 快適範囲
+			}
+			else
+			{
+				_score = pressureDiff; // 気圧差をそのままスコアとして使用
+			}
+
+			_OnResultGenerated(new Result(_score, _unit));
 		}
-		else
+
+		protected override void OnSensorDecided()
 		{
-			_score = pressureDiff; // 気圧差をそのままスコアとして使用
+			base.OnSensorDecided();
+			pressureSensor = (IPressureSensor)sensorManager.GetSensor();
 		}
 
-		_OnResultGenerated(new Result(_score, _unit));
-	}
-
-	protected override void OnSensorDecided()
-	{
-		base.OnSensorDecided();
-		pressureSensor = (IPressureSensor)sensorManager.GetSensor();
-	}
-
-	protected override void OnDeserializeCompleted()
-	{
-		GenerateEvaluationResult(pressureSensor);
+		protected override void OnDeserializeCompleted()
+		{
+			GenerateEvaluationResult(pressureSensor);
+		}
 	}
 }
