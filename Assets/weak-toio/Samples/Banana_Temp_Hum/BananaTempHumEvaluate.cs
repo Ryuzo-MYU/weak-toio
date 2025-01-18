5520008c238a5a89487cb519fd894fd31c9e6888
Ryuzo-MYU <p2120165@myu.ac.jp>
2025-01-18 04:18:18 +0000

    2025年1月18日 13:18

--- Assets/weak-toio/Samples/Banana_Temp_Hum/BananaTempHumEvaluate.cs
+++ Assets/weak-toio/Samples/Banana_Temp_Hum/BananaTempHumEvaluate.cs
@@ -7,8 +7,8 @@ namespace Evaluation
 	[RequireComponent(typeof(IHumiditySensor))]
 	public class BananaTempHumEvaluate : EvaluateBase
 	{
-		[SerializeField] private BoundaryRange suitableTempRange = new BoundaryRange(14, 20);
-		[SerializeField] private BoundaryRange suitableHumidRange = new BoundaryRange(45, 85); // 「なお45～85%の湿度範囲を常温という」https://kikakurui.com/z8/Z8703-1983-01.html
+		[SerializeField] private BoundaryRange suitableTempRange = new BoundaryRange(14, 20); // https://www.dole.co.jp/lp/jp/magazine/banana/preservation/#a03
+		[SerializeField] private BoundaryRange suitableHumidRange = new BoundaryRange(45, 85); // 「なお45～85%の湿度範囲を常温という」https://kikakurui.com/z8/Z8703-1983-01.html p.2
 		private ITemperatureSensor tempSensor;
 		private IHumiditySensor humiditySensor;
 
