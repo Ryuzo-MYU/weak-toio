abc4ff24445ec6f64ef00fc12755eae9b73b9e71
Ryuzo-MYU <p2120165@myu.ac.jp>
2025-01-18 04:01:29 +0000

    2025年1月18日 13:01

--- Assets/weak-toio/Samples/Glass_CO2/GrassCO2Evaluate.cs
+++ Assets/weak-toio/Samples/Glass_CO2/GrassCO2Evaluate.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 namespace Evaluation
 {
 	[RequireComponent(typeof(ICO2Sensor))]
-	public class GrassCO2Evaluate : EvaluateBase, IEvaluationResultSender<ICO2Sensor>
+	public class GrassCO2Evaluate : EvaluateBase
 	{
 		[SerializeField] private float OPTIMAL_CO2 = 400.0f;    // 通常大気のCO2濃度(ppm)
 		[SerializeField] private float MAX_EFFECT = 1200.0f;    // 光合成が最大になるCO2濃度(ppm)
 		private ICO2Sensor co2Sensor;
 
-		public void GenerateEvaluationResult(ICO2Sensor co2Sensor)
+		protected override void GenerateEvaluationResult()
 		{
 			_currentParam = co2Sensor.GetCO2();
 
@@ -38,7 +38,7 @@ namespace Evaluation
 
 		protected override void OnDeserializeCompleted()
 		{
-			GenerateEvaluationResult(co2Sensor);
+			GenerateEvaluationResult();
 		}
 	}
 }
