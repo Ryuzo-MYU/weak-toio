322196dd29b17bfed6f9ddf8ec272e2337c2fde8
Ryuzo-MYU <p2120165@myu.ac.jp>
2025-01-20 04:27:45 +0000

    2025年1月20日 13:27

--- Assets/weak-toio/Samples/Banana_Temp_Hum/BananaTempHumActionGenerator.cs
+++ Assets/weak-toio/Samples/Banana_Temp_Hum/BananaTempHumActionGenerator.cs
@@ -3,27 +3,27 @@ using Evaluation;
 
 namespace ActionGenerate
 {
-  public class BananaTempHumActionGenerator : ActionGenerator
-  {
-    BoundaryRange normalRange = new BoundaryRange(-3, 3);    // 正常範囲
-    BoundaryRange warningRange = new BoundaryRange(-6, 6);   // 警戒範囲
+	public class BananaTempHumActionGenerator : ActionGenerator
+	{
+		BoundaryRange normalRange = new BoundaryRange(0, 3);    // 正常範囲
+		BoundaryRange warningRange = new BoundaryRange(3, 6);   // 警戒範囲
 
-    protected override Action GenerateAction(Result result)
-    {
-      float score = result.Score;
+		protected override Action GenerateAction(Result result)
+		{
+			float score = result.Score;
 
-      if (normalRange.isWithInRange(score))
-      {
-        return actionLibrary.Banana_Temp_Hum_Normal();
-      }
-      else if (warningRange.isWithInRange(score))
-      {
-        return actionLibrary.Banana_Temp_Hum_Warning();
-      }
-      else
-      {
-        return actionLibrary.Banana_Temp_Hum_Rotting();
-      }
-    }
-  }
+			if (normalRange.isWithInRange(score))
+			{
+				return actionLibrary.Banana_Temp_Hum_Normal();
+			}
+			else if (warningRange.isWithInRange(score))
+			{
+				return actionLibrary.Banana_Temp_Hum_Warning();
+			}
+			else
+			{
+				return actionLibrary.Banana_Temp_Hum_Rotting();
+			}
+		}
+	}
 }
