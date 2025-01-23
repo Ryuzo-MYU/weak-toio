53b10dd2aec55a6393220d422a973ba4cb8170f6
Ryuzo-MYU <p2120165@myu.ac.jp>
2025-01-23 09:58:25 +0000

    2025年1月23日 18:58

--- Assets/weak-toio/Samples/Clothes_Hum/Clothes_Hum.cs
+++ Assets/weak-toio/Samples/Clothes_Hum/Clothes_Hum.cs
@@ -2,30 +2,33 @@ using ActionGenerate;
 
 namespace ActionLibrary
 {
-  public static class Clothes_Hum
-  {
-    public static Action Clothes_Hum_Good(this ToioActionLibrary lib)
-    {
-      Action action = new Action();
-      IMovementCommand movement = lib.Translate(100, 60);
-      IMovementCommand rotate = lib.DegRotate(360*2, 2);
+	public static class Clothes_Hum
+	{
+		public static Action Clothes_Hum_Good(this ToioActionLibrary lib)
+		{
+			Action action = new Action();
+			IMovementCommand movement = lib.Translate(100, 60);
+			IMovementCommand rotate = lib.DegRotate(360 * 2, 2);
 
-      return action;
-    }
+			return action;
+		}
 
-    public static Action Clothes_Hum_Normal(this ToioActionLibrary lib)
-    {
-      Action action = new Action();
-	
-      return action;
-    }
+		public static Action Clothes_Hum_Normal(this ToioActionLibrary lib)
+		{
+			Action action = new Action();
+			IMovementCommand movement = lib.Translate(60, 45);
+			IMovementCommand rotate = lib.DegRotate(180, 1);
+			action.AddMovement(movement);
+			action.AddMovement(rotate);
+			return action;
+		}
 
-    public static Action Clothes_Hum_Bad(this ToioActionLibrary lib)
-    {
-      Action action = new Action();
-      IMovementCommand movement = lib.Translate(30, 30);
+		public static Action Clothes_Hum_Bad(this ToioActionLibrary lib)
+		{
+			Action action = new Action();
+			IMovementCommand movement = lib.Translate(30, 30);
 
-      return action;
-    }
-  }
+			return action;
+		}
+	}
 }
