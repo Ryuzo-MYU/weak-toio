9f39ab912d4ebb80335371fd5c1a7a449471cf91
Ryuzo-MYU <p2120165@myu.ac.jp>
2025-01-28 08:54:35 +0000

    2025年1月28日 17:54

--- Assets/weak-toio/Samples/Human_Temp/Human_Temp.cs
+++ Assets/weak-toio/Samples/Human_Temp/Human_Temp.cs
@@ -2,19 +2,22 @@ using ActionLibrary;
 
 namespace ActionGenerate
 {
-  public static class Human_Temp
-  {
-    public static Action Human_Temp_Suitable(this ToioActionLibrary library)
-    {
-      throw new System.NotImplementedException();
-    }
-    public static Action Human_Temp_Hot(this ToioActionLibrary library)
-    {
-      throw new System.NotImplementedException();
-    }
-    public static Action Human_Temp_Cold(this ToioActionLibrary library)
-    {
-      throw new System.NotImplementedException();
-    }
-  }
+	public static class Human_Temp
+	{
+		public static Action Human_Temp_Suitable(this ToioActionLibrary library)
+		{
+			// あたりを歩き回るような，直線的な移動
+			throw new System.NotImplementedException();
+		}
+		public static Action Human_Temp_Hot(this ToioActionLibrary library)
+		{
+			// 暑さに苛立って激しく動き回る
+			throw new System.NotImplementedException();
+		}
+		public static Action Human_Temp_Cold(this ToioActionLibrary library)
+		{
+			// 時折蛇行しながら寒さで小刻みに震える
+			throw new System.NotImplementedException();
+		}
+	}
 }
