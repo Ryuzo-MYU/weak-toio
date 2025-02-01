0ac85d974c4c1d2749c5140dcacbca3f2ae81c79
Ryuzo-MYU <p2120165@myu.ac.jp>
2025-02-01 08:21:54 +0000

    2025年2月1日 17:21

--- Assets/weak-toio/Samples/Banana_Temp_Hum/BananaTempHumActionGenerator.cs
+++ Assets/weak-toio/Samples/Banana_Temp_Hum/BananaTempHumActionGenerator.cs
@@ -5,6 +5,11 @@ namespace ActionGenerate
 {
 	public class BananaTempHumActionGenerator : ActionGenerator
 	{
+		/// スコア解釈
+		/// 0：最適な環境
+		/// 0-10：軽度の環境逸脱
+		/// 10-20：中度の環境逸脱
+		/// 20以上：深刻な環境逸脱
 		BoundaryRange normalRange = new BoundaryRange(0, 10);    // 正常範囲
 		BoundaryRange warningRange = new BoundaryRange(10, 20);   // 警戒範囲
 
